5e77aa7e42bdc108315739c0209a74cb136d32f0
Marcospaulo-paixao <contato.marcospaulo1@gmail.com>
2019-10-02 00:40:23 -0300

    Lista de Exercicios N2

---
+++ Lista_de_Exercicios_N2/Exercicio01.java
@@ -0,0 +1,44 @@
+// CURSO:.Análise e Desenvolvimento de Sistemas
+// DISCIPLINA:.Algoritmos e Programação
+// ALUNO:.Marcos Paulo Pereira da Paixão
+// Lista de Exercícios – N2 
+//
+// 1. Faça um programa que leia dois números inteiros, o primeiro é o valor inicial de um
+// contador, e o segundo é o valor final do contador (testar se o valor inicial fornecido é
+// inferior ao valor final). Usando o comando While, escreva na tela uma contagem que
+// comece no primeiro número lido, escreva os números seguintes colocando sempre
+// apenas um número em cada nova linha da tela, e terminando a contagem quando chegar
+// ao valor final indicado.
+
+package Lista_de_Exercicios_N2;
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        String txt1 = "Informe o valor inicial do contador:";
+        String txt2 = "Informe o valor final do contador:";
+
+        int cont;
+
+        System.out.printf("%s\n", txt1);
+        int contIn = entrada.nextInt();
+
+        System.out.printf("%s\n", txt2);
+        int contFin = entrada.nextInt();
+
+        if (contIn > contFin) {
+            cont = contIn;
+            contIn = contFin;
+            contFin = cont;
+        }
+
+        while (contIn != contFin) {
+            System.out.printf("%d\n", contIn);
+            contIn++;
+        }
+        System.out.printf("%d\n", contIn);
+    }
+}
---
+++ Lista_de_Exercicios_N2/Exercicio02.java
@@ -0,0 +1,51 @@
+// CURSO:.Análise e Desenvolvimento de Sistemas
+// DISCIPLINA:.Algoritmos e Programação
+// ALUNO:.Marcos Paulo Pereira da Paixão
+// Lista de Exercícios – N2 
+//
+// 2. Escreva um programa que receba a idade de várias pessoas (para teste utilize 10
+// pessoas). Calcule e imprima: a quantidade de pessoas com até 18 anos; quantidade de
+// pessoas entre 18 e 65 anos; quantidade de pessoas com mais de 65 anos.
+//
+package Lista_de_Exercicios_N2;
+
+import java.util.Scanner;
+
+public class Exercicio02 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        String txt1 = "Informe a sua idade:";
+        String txt2 = "A idade Informada é Invalida!";
+        String txt3 = "pessoas tem até 18 anos.";
+        String txt4 = "pessoas tem idade entre 18 e 65 anos.";
+        String txt5 = "pessoas tem idade acima de 65 anos.";
+
+        int qntPessoa18 = 0;
+        int qntPessoa18e65 = 0;
+        int qntPessoa65 = 0;
+        int cont = 1;
+        int idade;
+
+        while (cont != 10) {
+            System.out.printf("%s\n", txt1);
+            idade = entrada.nextInt();
+            while (idade <= 0) {
+                System.out.printf("%s\n%s\n", txt2, txt1);
+            }
+            if (idade <= 18) {
+                qntPessoa18++;
+            } else if (idade > 18 && idade < 65) {
+                qntPessoa18e65++;
+            } else if (idade > 65) {
+                qntPessoa65++;
+            }
+
+            cont++;
+        }
+        System.out.printf("%d %s\n", qntPessoa18, txt3);
+        System.out.printf("%d %s\n", qntPessoa18e65, txt4);
+        System.out.printf("%d %s\n", qntPessoa65, txt5);
+    }
+
+}
