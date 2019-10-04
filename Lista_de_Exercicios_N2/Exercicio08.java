33de1ad227bcd5679d0b6b10173a3178923a780c
Marcospaulo-paixao <contato.marcospaulo1@gmail.com>
2019-10-04 00:39:58 -0300

    Lista de Exercício N2

---
+++ Lista_de_Exercicios_N2/Exercicio08.java
@@ -0,0 +1,51 @@
+/**
+ * CURSO:.Análise e Desenvolvimento de Sistemas
+ * DISCIPLINA:.Algoritmos e Programação
+ * ALUNO:.Marcos Paulo Pereira da Paixão
+ * Lista de Exercícios – N2
+ *
+ *
+ * 8. Escrever um programa que calcule todos os números inteiros divisíveis por um certo
+ * valor indicado pelo usuário, e compreendidos em um intervalo também especificado
+ * pelo usuário. O usuário deve entrar com um primeiro valor correspondente ao divisor e
+ * após ele vai fornecer o valor inicial do intervalo, seguido do valor final deste intervalo.
+ * Usar o comando While. Exemplo: Valor do divisor: 3; Inicio do Intervalo: 17; Fim do
+ * Intervalo: 29; Saída-> Números divisíveis por 3 no intervalo de 17 a 29: 18, 21, 24, 27.
+ */
+package Lista_de_Exercicios_N2;
+
+import java.util.Scanner;
+
+public class Exercicio08 {
+
+    public static void main(String[] args) {
+
+        // Variaveis
+        Scanner entrada = new Scanner(System.in);
+        int divisor, inicial, fim;
+        String divisiveis = "";
+
+        // Entrada
+        System.out.printf("Informe o divisor:");
+        divisor = entrada.nextInt();
+
+        System.out.printf("Informe o numero inicial:");
+        inicial = entrada.nextInt();
+        int iniAux = inicial;
+
+        System.out.printf("Informe o numero final:");
+        fim = entrada.nextInt();
+
+        // Processamento
+        while (inicial <= fim) {
+            if (inicial % divisor == 0) {
+                divisiveis += Integer.toString(inicial) + ", ";
+            }
+            inicial++;
+        }
+        // Saida
+        divisiveis = divisiveis.substring(0, divisiveis.length() - 2);
+        System.out.println("Números divisíveis por " + divisor + " no intervalo de " + iniAux + " a " + fim + ": " + divisiveis + ".");
+    }
+
+}
